154b914ba59567fb285e3989b479bbe6e3b07cb1
bahmansadiq <bahman.sadiq@gmail.com>
2017-02-15 19:21:57 -0800

    Passport authentication with jwt

---
+++ config/main.js
@@ -0,0 +1,10 @@
+module.exports = {  
+  // Secret key for JWT signing and encryption
+    'secret'   : 'i0loveC@r$',
+    'database' : 'mongodb://zas:password@ds157078.mlab.com:57078/zasmotors',
+
+  // Setting port for server
+  'port': process.env.PORT || 3000
+}
+
+
